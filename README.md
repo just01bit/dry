f3aa66db1ef75ee8bdb876c36bcbc680e5dcb92e
just01bit <ldai3@outlook.com>
2022-04-04 20:33:39 +1000

    update DRy.class to test URL query

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # dry
 DRY Utility
 ## About The Project
+This is a command line tool to improve efficiency of daily tasks.
+The first function is to return description and meaning of a word.
 
 ## Getting Started
 
--- src/DRY.java
+++ src/DRY.java
@@ -1,3 +1,19 @@
+import java.net.URL;
+import java.io.InputStream;
+import java.io.BufferedReader;
+
 public class DRY {
-	
+	public static void main(String[] args) {
+		String urlStr = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+		try 
+		{
+			URL url = new URL(urlStr);
+			InputStream is = url.openStream();
+			BufferedReader buffer = new BufferedReader(is);			
+			System.out.printf(buffer.toString());
+		} 
+		catch (Exception e) {
+
+		}
+	}
 }
