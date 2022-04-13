d40d14f393d66f9b59a978f930b2bb31cec3295b
just01bit <ldai3@outlook.com>
2022-04-13 20:42:03 +1000

    TODO: JSON parser

--- src/DRY.java
+++ src/DRY.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class DRY {
 	public static void main(String[] args) {
-		String urlStr = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+		String urlStr = "https://api.dictionaryapi.dev/api/v2/entries/en/" + args[0];
 		try 
 		{
 			URL url = new URL(urlStr);
@@ -13,6 +13,8 @@ public class DRY {
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader buffer = new BufferedReader(isr);	// this does not work		
 			System.out.printf(buffer.readLine());
+
+			// TODO: JSON parser
 		} 
 		catch (Exception e) {
 			System.out.printf(e.getMessage());
