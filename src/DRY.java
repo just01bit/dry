0d4b2dde280ec36ab90f3fb7b048d05fb4ec4983
just01bit <ldai3@outlook.com>
2022-05-01 15:35:27 +1000

    add 2 more functions

--- src/DRY.java
+++ src/DRY.java
@@ -2,10 +2,59 @@ import java.net.URL;
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.FileInputStream;
 
 public class DRY {
 	public static void main(String[] args) {
-		String urlStr = "https://api.dictionaryapi.dev/api/v2/entries/en/" + args[0];
+		String functionType = "";
+		if (args.length > 0) {
+			functionType = args[0];
+		}
+
+		if(functionType.equals("d")) {
+			dictionary(args[1]);
+		} else if(functionType.equals("rtf")){
+			rtfLog2JSON(args[1]);
+		}
+
+	}
+
+	/*
+		convert rtf log to JSON 
+		refer to rtfLogJSONSample.json
+	*/
+	private static void rtfLog2JSON(String filePath){
+		filePath = "C:\\Users\\ldai3\\OneDrive\\Desktop\\nodesoutput_20220428";
+		String outputJSON = "";
+		try 
+		{
+			FileInputStream fileInputStream = new FileInputStream(filePath);
+			InputStreamReader isr = new InputStreamReader(fileInputStream);	
+			BufferedReader buffer = new BufferedReader(isr);
+
+			System.out.println(splitStr(buffer.readLine())[1]);
+			System.out.println(buffer.readLine());
+			System.out.println(buffer.readLine());
+
+
+		} catch (Exception e) {
+			System.out.printf(e.getMessage());
+		}
+
+	}
+
+	/* 
+		split string by whitespace and return array
+	*/
+	private static String[] splitStr(String s) {
+		return s.split("\\s+");
+	}
+
+	/* 
+		search word definition
+	*/
+	private static void dictionary(String word) {
+		String urlStr = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
 		try 
 		{
 			URL url = new URL(urlStr);
